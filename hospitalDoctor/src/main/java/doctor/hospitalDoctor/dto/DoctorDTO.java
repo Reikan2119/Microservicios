23101e886534cfb7f3558da343bd4e03dbd14d1f
Mapache <cristh.silvam@duocuc.cl>
2026-06-16 18:21:18 -0400

    Add files via upload

---
+++ hospitalDoctor/src/main/java/doctor/hospitalDoctor/dto/DoctorDTO.java
@@ -0,0 +1,13 @@
+package doctor.hospitalDoctor.dto;
+
+
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class DoctorDTO {
+    private Integer id;
+    private String nombreDoctor;
+    private String especialidad;
+}
