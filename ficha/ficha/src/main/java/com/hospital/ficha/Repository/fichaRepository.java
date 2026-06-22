88dfb2da1c41112d02fb1035ea62488246ea4fe8
Mapache <cristh.silvam@duocuc.cl>
2026-06-22 17:50:18 -0400

    Delete ficha/ficha directory

--- ficha/ficha/src/main/java/com/hospital/ficha/Repository/fichaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.hospital.ficha.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.hospital.ficha.Model.fichaModel;
-
-public interface fichaRepository extends JpaRepository<fichaModel, Integer>{
-
-}
